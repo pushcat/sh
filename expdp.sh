bb259ce30567c316265b38a4d55656b83b8a92a9
pushcat <charlesby@hotmail.com>
2017-07-14 17:09:40 +0800

    update script

--- expdp.sh
+++ expdp.sh
@@ -1,3 +1,10 @@
-#!bin/bash
-cti=`date +%Y%m%d%H%M`
-expdp scott/tiger@prod schemas=scott dumpfile=expdp-$cti.dmp directory=dpdata1
+#!/bin/bash
+# configure the environment variables
+export ORACLE_BASE=/u01/app/oracle
+export ORACLE_HOME=$ORACLE_BASE/product/11.2.0/dbhome_1
+export ORACLE_SID=PROD
+export PATH=$ORACLE_HOME/bin:$PATH
+
+# backup script and filename format
+dt=`date +%Y%m%d%H%M`
+expdp scott/tiger@prod schemas=scott dumpfile=expdp-$dt.dmp logfile=expdp-$dt.log directory=dpdata1
